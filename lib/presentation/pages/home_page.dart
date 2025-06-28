4926ade9c10c1abee4122c77a8a48bcef53229fb
Shahanas <shahanasov@gmail.com>
2025-06-28 12:12:50 +0530

    homeUIcompleted

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -34,7 +34,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Own a Pet')),
+      appBar: AppBar(
+        title: const Text('Own a Pet'),
+        actions: [
+          IconButton.outlined(onPressed: () {}, icon: const Icon(Icons.star)),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Obx(() {
@@ -46,70 +51,141 @@ class HomePage extends StatelessWidget {
             children: [
               GestureDetector(
                 onTap: () => Get.to(() => AdoptPage()),
-
-                child: Card(elevation: 5,
+                child: Card(
+                  elevation: 5,
                   color: Colors.white,
                   child: SizedBox(
                     height: 150,
                     width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pets are awaiting\n    to be Loved',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            spacing: 0,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('🐶', style: TextStyle(fontSize: 25)),
+                              Text(
+                                'Found a pet you like?',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(10),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+
+                                child: const Text(
+                                  'Apply to Adopt ',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                onPressed: () {},
+                              ),
+                            ],
                           ),
-                        ),
-                        Text('Adopt a pet')
-                      ],
+                          Image.asset('asset/adopt-us.jpg', width: 200),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 25,),
+              const SizedBox(height: 25),
               Expanded(
                 child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 2,
-                    mainAxisSpacing: 5,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
                     crossAxisCount: 2,
+                    childAspectRatio: 0.8,
                   ),
                   itemCount: controller.pets.length,
                   itemBuilder: (context, index) {
                     final pet = controller.pets[index];
                     return Card(
                       elevation: 2,
-                      child: Column(
-                        children: [
-                          Stack(
-                            children: [
-                              !pet.isFriendly
-                                  ? const Icon(Icons.star_border)
-                                  : const Icon(Icons.star_purple500_outlined),
-                              SizedBox(
-                                height: 100,
-                                width: 150,
-                                child: Image.network(pet.petImage),
-                              ),
-                            ],
-                          ),
-                          ListTile(
-                            title: Text(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Stack(
+                              alignment: Alignment.topRight,
+                              children: [
+                                SizedBox(
+                                  height: 120,
+                                  width: double.infinity,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.asset(
+                                      'asset/profiledog.jpg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                pet.isFriendly
+                                    ? IconButton(
+                                      icon: const Icon(
+                                        Icons.star,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        // Handle favorite action
+                                      },
+                                      style: IconButton.styleFrom(
+                                        backgroundColor: Colors.white
+                                            .withValues(
+                                              alpha: (0.2),
+                                            ), // THIS gives real transparency
+                                      ),
+                                    )
+                                    : IconButton(
+                                      icon: const Icon(
+                                        Icons.star_border,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        // Handle favorite action
+                                      },
+                                      style: IconButton.styleFrom(
+                                        backgroundColor: Colors.white
+                                            .withValues(
+                                              alpha: (0.2),
+                                            ), // ~20% opacity
+                                        // THIS gives real transparency
+                                      ),
+                                    ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
                               pet.petName,
                               style: const TextStyle(
                                 fontSize: 18,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            subtitle: Text(
+
+                            Text(
                               pet.userName,
-                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     );
                   },
